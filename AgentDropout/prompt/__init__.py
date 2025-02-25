0fe512681a453c564fb9215b86653df9fe1515ee
zhexuan wang <1042545513@qq.com>
2025-02-25 22:24:59 +0800

    Add files via upload

--- AgentDropout/prompt/__init__.py
+++ AgentDropout/prompt/__init__.py
@@ -1,10 +1,10 @@
-from AgentPrune.prompt.prompt_set_registry import PromptSetRegistry
-from AgentPrune.prompt.mmlu_prompt_set import MMLUPromptSet
-from AgentPrune.prompt.humaneval_prompt_set import HumanEvalPromptSet
-from AgentPrune.prompt.gsm8k_prompt_set import GSM8KPromptSet
-from AgentPrune.prompt.aqua_prompt_set import AQUAPromptSet
-from AgentPrune.prompt.math_prompt_set import MathPromptSet
-from AgentPrune.prompt.mathc_prompt_set import MathcPromptSet
+from AgentDropout.prompt.prompt_set_registry import PromptSetRegistry
+from AgentDropout.prompt.mmlu_prompt_set import MMLUPromptSet
+from AgentDropout.prompt.humaneval_prompt_set import HumanEvalPromptSet
+from AgentDropout.prompt.gsm8k_prompt_set import GSM8KPromptSet
+from AgentDropout.prompt.aqua_prompt_set import AQUAPromptSet
+from AgentDropout.prompt.math_prompt_set import MathPromptSet
+from AgentDropout.prompt.mathc_prompt_set import MathcPromptSet
 
 __all__ = ['MMLUPromptSet',
            'HumanEvalPromptSet',
